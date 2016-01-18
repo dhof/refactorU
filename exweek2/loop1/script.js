ba528bf055620f9ec4d03f05febaafc6fcf37335
D <D@D-laptop-2.local>
2016-01-18 11:32:16 -0700

    completed loop practice 1

---
+++ exweek2/loop1/script.js
@@ -0,0 +1,31 @@
+// for (var i = 0; i <= 500; i += 100) {
+// 	console.log(i);
+// };
+
+// for (var i = 1; i <= 64; i *= 2) {
+// 	console.log(i);
+// };
+
+// for (var i = 1; i <= 3; i ++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// };
+
+// for (var i = 0; i <= 10; i += 2) {
+// 	console.log(i);
+// };
+
+// for (var i = 3; i <= 15; i += 3) {
+// 	console.log(i);
+// };
+
+// for (var i = 9; i >= 0; i--) {
+// 	console.log(i);
+// };
+
+for (var y = 1; y <= 3; y++) {
+	for (var i = 0; i <= 3; i ++) {
+	console.log(i);
+	};
+};
