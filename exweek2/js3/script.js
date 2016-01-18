7cae05087184610d1e828c3a0ab4e3081b25d0fd
D <D@D-laptop-2.local>
2016-01-18 12:31:27 -0700

    completed js3

---
+++ exweek2/js3/script.js
@@ -0,0 +1,46 @@
+// var usernumber = prompt("Please enter your phone number in the following format: 555-555-5555");
+
+// if (usernumber[3] === '-' && usernumber[7] === '-') {
+// 	alert('that was easy');
+// }
+// else {
+// 	alert('you don't trust me?'')
+// };
+
+// var userbday = prompt("Please enter your birthday in the following format: xx/xx/xx");
+
+// if (userbday[2] === '/' && userbday[5] === '/') {
+// 	alert('that was easy');
+// }
+// else {
+// 	alert("you don't trust me?")
+// };
+
+// var userzip = prompt("Please enter your zipcode in the following format: 12345 or 12345-1234");
+
+// if (userzip.length === 5 || userzip[5] === '-') {
+// 	alert('that was easy');
+// }
+// else {
+// 	alert("you don't trust me?")
+// };
+
+// var usercity = prompt("Please enter the abbreviation of the city you live in. Ex: MD, CA, FL");
+
+// if (usercity.length === 2 && isNaN(usercity) && usercity == usercity.toUpperCase()) {
+// 	alert('that was easy');
+// }
+// else {
+// 	alert("you don't trust me?")
+// };
+
+
+var usermarried = prompt("Are you married? Please enter in the following formats: YES, Yes, yes, NO, No, no");
+
+
+if (usermarried === 'YES' || usermarried === 'Yes' || usermarried === 'yes' || usermarried === 'NO' || usermarried === 'No' || usermarried === 'no') {
+	alert('that was easy');
+}
+else {
+	alert("you don't trust me?")
+};
