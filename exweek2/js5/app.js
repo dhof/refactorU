565644c4a21b6bdcb8e86bd30ac3e9abdd234d27
D <D@D-laptop-2.local>
2016-01-21 14:21:06 -0700

    completed js5

--- exweek2/js5/app.js
+++ exweek2/js5/app.js
@@ -1,18 +1,43 @@
 angular.module('blogSite', []);
-angular.module('blogSite').controller('blogFunc', ['$scope', function($scope) {
+angular.module('blogSite').controller('blogFunc', ['$scope', '$timeout', function($scope, $timeout) {
+	var focusElement1 = document.getElementById("inputNumber1");
+	var focusElement2 = document.getElementsByClassName("inputNumber2")[0];
+	$scope.descript = "Blog o' the rando dude!";
+	$scope.bday = "dumpster";
+	// $scope.updateDescript = '';
+	// $scope.randoDescrip = function() {
+	// 	$scope.descript = $scope.updateDescript;
+	// }
+
 	$scope.showingElement1 = true;
 	$scope.change1 = function() {
 		$scope.showingElement1 = false;
+		$timeout(function(){
+			focusElement1.focus()
+		})
 	}
+
+	$scope.blur1 = function() {
+		$scope.showingElement1 = true;
+		$scope.descript = $scope.updateDescript;
+	}
+
 	$scope.showingElement2 = true;
 	$scope.change2 = function() {
 		$scope.showingElement2 = false;
+		$timeout(function(){
+			focusElement2.focus()
+		}
+		, 0)
 	}
-	
-	var Devon = document.getElementById("devon")
 
-	$timeout(function(){
-	Devon.focus()
-})
+	$scope.blur2 = function() {
+		$scope.showingElement2 = true;
+		$scope.bday = $scope.updateBday;
+	}
+
+	
+
+	
 }]);
 
