e977fdfc148d29f71365b365aaa40aa00d9b4d9a
dhof <devon_hof@yahoo.com>
2016-01-28 16:55:58 -0700

    completed functional Practice 2 up to 10

--- exweek3/funcPrac2/script.js
+++ exweek3/funcPrac2/script.js
@@ -20,8 +20,97 @@
 
 // console.log(keyValue('color', 'green'));
 
-var negativeIndex = function(array, position) {
-	return array[array.length + position];
-}
+// var negativeIndex = function(array, position) {
+// 	return array[array.length + position];
+// }
 
-console.log(negativeIndex(['dog','cat','bat','tiger'], -1));
+// console.log(negativeIndex(['dog','cat','bat','tiger'], -1));
+
+// var removeM = function(string) {
+// 	var splitMe = string.split("m");
+// 	var joined = splitMe.join("");
+// 	return joined;
+	
+// }
+
+// console.log(removeM("mammal there guy man"));
+
+
+// var printObject = function(obj) {
+// 	for (var prop in obj) {
+// 		console.log(prop + " is " + obj[prop]);
+// 	}
+// }
+
+// printObject({a: 10, b: 20, c: 30})
+
+// var vowels = function(string) {
+// 	var vowelOutput = [];
+// 	for(i = 0; i < string.length; i++) {
+// 		if(string[i] === 'a' || string[i] === 'e' || string[i] === 'i' || string[i] === 'o' || string[i] === 'u' || string[i] === 'y') {
+// 		vowelOutput.push(string[i]);
+// 		}
+// 	}
+// 	return vowelOutput;
+// }
+
+// console.log(vowels('i like me some vowels'));
+
+
+// var twins = function(array) { // better way is to push pairs into an empty array and check that array
+	
+// 	for(i = 0; i < array.length; i += 2) {
+// 		var a = i;
+// 		var b = i + 1;
+// 		var truthy = false
+		
+// 	    if (array[a] === array[b]) {	
+// 	    	truthy = true
+// 		} else {
+// 			truthy = false
+// 		}
+// 	}
+// 	console.log(truthy);
+// }
+
+// twins(['a', 'a', 'c', 'c', 'e', 'd']);
+
+
+// var or = function(array) {
+// 	var truthy = false;
+// 	for(i = 0; i < array.length; i++) {
+// 		if(array[i] === true) {
+// 			truthy = true;
+// 			break;
+// 		} 
+// 	}
+// 	return truthy;
+// }
+
+// console.log(or([false, true, false, false]));
+
+
+
+// var unique = function(array) {
+// 	var uniqueStrings = [];
+// 	for(i = 0; i < array.length; i++) {
+// 		if 
+// 	}
+// }
+
+
+// var duplicatesArray = ['a', 'b', 'd', 'c', 'f', 'c']
+
+// var unique = duplicatesArray.filter(function(element) {
+// 	var uniqueArray = [];
+// 	for(i = 0; i < duplicatesArray.length; i++) {
+		
+// 	}
+// })
+
+
+
+
+
+
+
