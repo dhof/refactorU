f29a8c697e0e61e1df54bb73386d87a83d03aba5
dhof <devon_hof@yahoo.com>
2016-01-29 15:31:45 -0700

    completed Problem Set 2 up to #3

--- exweek3/probSet2/script.js
+++ exweek3/probSet2/script.js
@@ -6,22 +6,78 @@
 // console.log(firstReverse('hello'));
 
 
-var swapCase = function(string) {
-	var split = string.split(' ');
-	for(i = 0; i < split.length; i++) {
-		console.log(split)
-		// console.log(split[i])
-		for(j = 0; j < split[i].length; j++) {
-			console.log(split[i].split[j])
-			if(j === 0) {
-				// console.log(split[j]);
-				split[j].toLowerCase();
-			} else {
-				split[j].toUpperCase();
-			}
-		}
-}
-	// console.log(split)
+
+
+//attempt at swapCase while not assuming perfect casing
+// var swapCase = function(string) {
+// 	var result = string.split(' ').map(function(thisArg){
+// 		var stringy = '';
+// 		var splitArg = thisArg.split('')
+// 		// console.log("splitArg : " + splitArg);
+
+// 		for(i = 0; i < thisArg.length; i++) {
+// 			if (i === 0) {
+// 				stringy += thisArg[i].toLowerCase();
+
+// 			} else {
+// 				stringy += splitArg[i].toUpperCase();
+// 			}
+// 			console.log(stringy);
+// 		}
+// 		console.log(stringy);
+// 		console.log(thisArg);
+// 		console.log( joined);
+
+// 	})
+// 	// var joined = stringy.join(' ');
+// 	return joined;
+// }
+
+// console.log(swapCase('hello there dude guy'));
+
+
+
+// var swapCase = function(string) {
+// 	var stringy = '';
+// 	for (i = 0; i < string.length; i++) {
+// 		if(string[i] === string[i].toUpperCase()) {
+// 			stringy += string[i].toLowerCase();
+// 		} else {
+// 			stringy += string[i].toUpperCase();
+// 		}
+// 	}
+// 	return stringy;
+// }
+
+// console.log(swapCase('thIs iS a StriNg'));
+
+
+
+var letterCount = function(string) {
+	var splitString = string.split(' ');
+	var splitObjects = {};
+
+	for (i = 0; i < string.length; i++) {
+		
+	}
 }
 
-swapCase('hello there');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
