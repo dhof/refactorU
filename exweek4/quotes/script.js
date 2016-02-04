40edd4b5632bce2c795c1b41bae74942dc93f0db
dhof <devon_hof@yahoo.com>
2016-02-04 16:38:01 -0700

    added push and remove functionality to quote creation

--- exweek4/quotes/script.js
+++ exweek4/quotes/script.js
@@ -1,33 +1,48 @@
 var app = angular.module('inspirationalQuotes', []);
 
 app.controller('quotesControl', ['$scope', function($scope) {
+	var quoteTracker = 1;
 	$scope.quotes = [];
 	$scope.authorName = "";
 	$scope.quoteText = "";
+	$scope.removeTruthy = false;
 
 
-	$scope.quoteCreate = function(author, text, rating) {
+	$scope.QuoteCreate = function(author, text, rating) {
+		this.quoteId = quoteTracker;
 		this.author = author,
 		this.text = text,
 		this.rating = null;
+		quoteTracker++;
 		$scope.quotes.push(this);
+		$scope.newQuote = {};
 	}
 
-	var firstQuote = new $scope.quote (
-	"devon",
-	"Lorem ipsum dolor sit amet."
+	$scope.submitQuote = function() {
+		$scope.removeTruthy = true;
+	}
+
+	$scope.removeLast = function() {
+		$scope.quotes.pop();
+		console.log($scope.quotes);
+		$scope.removeTruthy = false;
+	}
+
+
+	var kanyeQuote1 = new $scope.QuoteCreate (
+		"Kanye West",
+		"I hate when I'm on a flight and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle."
 	)
 
-	var secondQuote = new $scope.quote (
-	"shten",
-	"bacon something something yum."
+	var anonQuote1 = new $scope.QuoteCreate (
+		"Anonymous",
+		"Sometimes when I close my eyes, I can't see."
 	)
 
-	var thirdQuote = new $scope.quote (
-	"the internet",
-	"How I mine for fish."
+	var kintzQuote1 = new $scope.QuoteCreate (
+		"Jarod Kintz",
+		"I want to write my own eulogy, and I want to write it in Latin. It seems only fitting to read a dead language at my funeral."
 	)
-
 
 
 }])
